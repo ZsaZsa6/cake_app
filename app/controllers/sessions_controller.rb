a46c420e2c5c2e7a237fc3cae680ce10fea8a650
Ronda Cowen <cowenhome5@gmail.com>
2020-11-30 09:27:42 -0600

    git commit second try

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+    def new
+
+    end 
+  
+    def create
+      if customer = Customer.find_by(username: params[:username])
+        if customer && customer.authenticate(params[:password])
+          session[:customer_id] = customer.id
+          
+          redirect_to customer_path(customer) 
+        end 
+      else  
+        flash[:alert] =  "Please try again or Sign Up."
+        redirect_to login_path
+      end
+    end
+
+    def destroy
+        session[:customer_id] = nil 
+        redirect_to login_path 
+    end 
+    
+end
